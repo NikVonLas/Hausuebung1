d5ed45be7c613c6696238a5fb0720fcb0ae921ae
Niklas <westermayer.niklas@gmail.com>
2023-09-24 20:42:48 +0200

    final
    final

--- Hausuebung1/src/EratosthenesPrimeSieve.java
+++ Hausuebung1/src/EratosthenesPrimeSieve.java
@@ -2,21 +2,22 @@ import java.util.ArrayList;
 
 public class EratosthenesPrimeSieve implements EratosthenesInterface {
     public int limit;
-    public ArrayList<Integer> primesList = new ArrayList<>();
+    public ArrayList<Integer> examples = new ArrayList<>();
     public ArrayList<Integer> numbersList = new ArrayList<>();
 
     @Override
     public boolean isPrime(int input) {
+        upToo(examples);
         upToo(numbersList);
-        for (int i = 0; i < numbersList.size(); i++) {
-            if (numbersList.get(i)==2&& numbersList.get(i)%2 == 0 ){
-                numbersList.remove(numbersList.get(i));
+        for (int i = 1; i < numbersList.size(); i++) {
+            if (numbersList.get(i)==2){
+                numbersList.remove(2);
                 System.out.println(numbersList);
 
             } else if (numbersList.get(i)>2) {
-                for (int j = 0; j < numbersList.size(); j++) {
-                    if(numbersList.get(i)%numbersList.get(j)== 0){
-                        numbersList.remove(j);
+                for (int j = 1; j < examples.size(); j++) {
+                    if(numbersList.get(i)%examples.get(j)== 0){
+                        numbersList.remove(examples.get(j));
                     }
                 }
 
--- Hausuebung1/src/Main.java
+++ Hausuebung1/src/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        while (true){
+        boolean loop = true;
+        while ( loop== true){
             System.out.println("obergrenze der zu Berechnenden Zahl eingeben");
             System.out.print(">>");
             int input1 = scanner.nextInt();
             EratosthenesPrimeSieve primeSieve = new EratosthenesPrimeSieve(input1);
             primeSieve.isPrime(input1);
+            loop = false;
         }
     }
 }
